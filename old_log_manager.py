44d8b46622af657dcb1810a6484faa9d0d278620
Semin Jeon <semini93@gmail.com>
2019-08-28 10:47:20 -0400

    Change methods for path

--- old_log_manager.py
+++ old_log_manager.py
@@ -12,20 +12,20 @@ from fnmatch import fnmatch
 4. Logging, .exit() return should be implemented
 """
 
-server_list  = ["front_A/", "front_B/", "app_A/", "app_B/"]
-sub_list = [["inst1/", "inst2/", "inst3/"], ["int1/", "int2/", "ext1/", "ext2/", "ext3/"]]
+server_list  = ["front_A", "front_B", "app_A", "app_B"]
+sub_list = [["inst1", "inst2", "inst3"], ["int1", "int2", "ext1", "ext2", "ext3"]]
 
 root_path = "#"
-extracted_path = root_path + "extracted/"
-stat_path = root_path + "stat/"
-archive_temp_path = root_path + "archive/temp/"
-archive_path = root_path + "archive/"
+extracted_path = os.path.join(root_path, "extracted")
+stat_path = os.path.join(root_path, "stat")
+archive_temp_path = os.path.join(root_path, "archive", "temp")
+archive_path = os.path.join(root_path, "archive")
 access_right = 0o755
 
 
 # Date folder in archive path (Archving yesterday logs)
 yesterday = date.today() - timedelta(1)
-yesterday_path = archive_path + yesterday.strftime("%d-%b-%Y/")
+yesterday_path = os.path.join(archive_path, yesterday.strftime("%d-%b-%Y/"))
 
 
 # Create whole folder structure (extracted -> archive /date/)
@@ -33,11 +33,11 @@ def create_folders(target_path, access_right):
 
   for server in range(0, 2, 1):
     for sub in range(0, 3, 1):
-      os.makedirs((target_path + server_list[server] + sub_list[0][sub]), access_right, exist_ok = True)
+      os.makedirs(os.path.join(target_path, server_list[server], sub_list[0][sub]), access_right, exist_ok = True)
   
   for server in range(2, 4, 1):
     for sub in range(0, 5, 1):
-      os.makedirs((target_path + server_list[server] + sub_list[1][sub]), access_right, exist_ok = True)
+      os.makedirs(os.path.join(target_path, server_list[server], sub_list[1][sub]), access_right, exist_ok = True)
 
 # end of create_folders
 
@@ -49,21 +49,23 @@ def move_old_logs(origin_path, target_path):
 
   for server in range(0, 2, 1):
     for sub in range(0, 3, 1):
-      file_list = os.listdir(origin_path + server_list[server] + sub_list[0][sub])
+      file_path = os.path.join(origin_path, server_list[server], sub_list[0][sub])
+      file_list = os.listdir(file_path)
       if len(file_list) != 0:
         for file in file_list:
-          move((origin_path + server_list[server] + sub_list[0][sub] + file), (target_path + server_list[server] + sub_list[0][sub] + file))
+          move(os.path.join(origin_path, server_list[server], sub_list[0][sub] + file), os.path.join(target_path, server_list[server], sub_list[0][sub], file))
       else:
-        os.rmdir(target_path + server_list[server] + sub_list[0][sub])
+        os.rmdir(os.path.join(target_path, server_list[server], sub_list[0][sub]))
   
   for server in range(2, 4, 1):
     for sub in range(0, 5, 1):
-      file_list = os.listdir(origin_path + server_list[server] + sub_list[0][sub])
+      file_path = os.path.join(origin_path + server_list[server] + sub_list[1][sub])
+      file_list = os.listdir(file_path)
       if len(file_list) != 0:
         for file in file_list:
-          move((origin_path + server_list[server] + sub_list[1][sub] + file), (target_path + server_list[server] + sub_list[1][sub] + file))
+          move(os.path.join(origin_path, server_list[server], sub_list[1][sub], file), os.path.join(target_path, server_list[server], sub_list[1][sub], file))
       else:
-        os.rmdir(target_path + server_list[server] + sub_list[1][sub])
+        os.rmdir(os.path.join(target_path, server_list[server], sub_list[1][sub]))
 
 
 # Clean up (delete) 'files only' recursively in targeted path
@@ -75,12 +77,12 @@ def clean_up_folders(target_path):
     for folder in os.walk(target_path):  # sets of tuples (o: folder, 1: [subfolders], 2: [files])
       for file in folder[2]:
         if fnmatch(file, old_2days_pattern):
-          os.remove(folder[0] + "/" + file)
+          os.remove(os.path.join(folder[0], file))
 
   else:  # Get rid of contents in any folder and its subfolders
     for folder in os.walk(target_path):
       for file in folder[2]:
-        os.remove(folder[0] + "/" + file)
+        os.remove(os.path.join(folder[0], file))
 
 # end of clean_up_folders
 
@@ -90,13 +92,12 @@ def clean_up_folders(target_path):
 def zip_Ndays_old_logs(N = 3):
 
   old_Ndays = date.today() - timedelta(N)
-  old_Ndays_path = archive_path + old_Ndays.strftime("%d-%b-%Y/")
+  old_Ndays_path = os.path.join(archive_path, old_Ndays.strftime("%d-%b-%Y/"))
 
   if os.path.exists(old_Ndays_path):
     make_archive(old_Ndays_path, 'zip', old_Ndays_path)
 
-    # slicing to get rid of '/' very end of path
-    if os.path.exists(old_Ndays_path[:-1] + '.zip'):
+    if os.path.exists(old_Ndays_path + '.zip'):
       rmtree(old_Ndays_path)
 
 # end of zip_Ndays_old_logs
@@ -109,7 +110,7 @@ def delete_oldest_log(N = 95):
 
   # Based on Date on folder name
   old_Ndays = date.today() - timedelta(N)
-  old_Ndays_path = archive_path + old_Ndays.strftime("%d-%b-%Y.zip")
+  old_Ndays_path = os.path.join(archive_path, old_Ndays.strftime("%d-%b-%Y.zip"))
   if os.path.exists(old_Ndays_path):
     os.remove(old_Ndays_path)
 
--- unzip.py
+++ unzip.py
@@ -9,29 +9,29 @@ from shutil import move  # To move files over
 """
 
 
-server_list  = ["front_A/", "front_B/", "app_A/", "app_B/"]
-sub_list = [["inst1/", "inst2/", "inst3/"], ["int1/", "int2/", "ext1/", "ext2/", "ext3/"]]
+server_list  = ["front_A", "front_B", "app_A", "app_B"]
+sub_list = [["inst1", "inst2", "inst3"], ["int1", "int2", "ext1", "ext2", "ext3"]]
 
 root_path = "#"
-extracted_path = root_path + "extracted/"
-stat_path = root_path + "stat/"
-archive_temp_path = root_path + "archive/temp/"
+extracted_path = os.path.join(root_path, "extracted")
+stat_path = os.path.join(root_path, "stat")
+archive_temp_path = os.path.join(root_path, "archive", "temp")
 
 
 def change_folder(server, sub, is_mobile):
   
-  landing_path = (root_path + "landing/") if is_mobile == False else (root_path + "landing/mobile/")
+  landing_path = os.path.join(root_path, "landing") if is_mobile == False else os.path.join(root_path, "landing", "mobile")
   paths = {}
-  paths['landing_path'] = (landing_path + server + sub)
-  paths['extratced_path'] = (landing_path + server + sub)
-  paths['stat_path'] = (stat_path + server + sub)
-  paths['archive_temp_path'] = (archive_temp_path + server + sub)
+  paths['landing_path'] = os.path.join(landing_path, server, sub)
+  paths['extratced_path'] = os.path.join(landing_path, server, sub)
+  paths['stat_path'] = os.path.join(stat_path, server, sub)
+  paths['archive_temp_path'] = os.path.join(archive_temp_path, server, sub)
   return paths
 
 
 def unzip_move(paths):
 
-  file_list = os.listdir(paths['landing_path'])  # Fix the number of task (TO prevent redundant job)
+  file_list = os.listdir(paths['landing_path'])  # Fix the number of task (To prevent redundant job)
 
   if len(file_list) != 0:
     
@@ -40,7 +40,8 @@ def unzip_move(paths):
 
     for file in file_list:
       # Extract .trz files (landing_path -> extracted_path)
-      tar = tarfile.open(paths['landing_path'] + file)
+      file_path = os.path.join(paths['landing_path'] + file)
+      tar = tarfile.open(file_path)
       tar.extractall(paths['extracted_path'])
       # Extract one more time for stats (landing_path -> stat_path)
       tar.extractall(paths['stat_path'])
@@ -48,12 +49,14 @@ def unzip_move(paths):
 
       # Move .trz files (landing_path -> archive_path)
       # shutil.move() using os.rename() and when the file system is different, does shutil.copy2() and removes sources
-      move(paths['landing_path'] + file, paths['archive_temp_path'] + file)
+      move(os.path.join(paths['landing_path'], file), os.path.join(paths['archive_temp_path'], file))
+    
     # end of for
 
   # end of if
 
 # end of unzip_move
+
 
 
 def main():
