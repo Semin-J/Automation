12d5bba06379f81151d5b4d7cc79b966cf473b48
Semin Jeon <semini93@gmail.com>
2019-09-10 16:07:12 -0400

    Created log management module

--- setup_logger.py
+++ setup_logger.py
@@ -1,4 +1,5 @@
-import logging
+import logging, os
+from datetime import date
 
 formatter = logging.Formatter(fmt = '%(asctime)s.%(msecs)03d|%(levelname)s|%(module)s|%(message)s',
                               datefmt = '%Y-%m-%d@%H:%M:%S')
@@ -15,3 +16,16 @@ def setup_logger(name, log_path, level = logging.INFO):
   logger.addHandler(handler)
 
   return logger
+
+# Create log files (Once created, append)
+batch_log_path = '#'
+today_pattern = date.today().strftime("%Y%m%d")
+
+today_debug_log_path = os.path.join(batch_log_path, (today_pattern + '_Debug.log'))
+today_info_error_log_path = os.path.join(batch_log_path, (today_pattern + '_Info_Error.log'))
+
+# This debug_log is logging from DEBUG level to above (But us it for DEBUG level only)
+debug_log = setup_logger('deb_log', today_debug_log_path, logging.DEBUG)
+
+# This info_error_log is logging from INFO level to above (But use it for INFO, ERROR level only)
+info_error_log = setup_logger('err_log', today_info_error_log_path, logging.INFO)
